a0ed4550d7ad377e618c85483f3384ffeb127bab
Jesal Vadgama <jesalvadgama@gmail.com>
2025-12-12 10:27:47 +0000

    bug: fixed moveable minipages scaling

--- app/components/moveable.tsx
+++ app/components/moveable.tsx
@@ -3,12 +3,10 @@ import { CSS } from '@dnd-kit/utilities';
 
 export default function Moveable({ id, children, movable = true }: { id: string; children: React.ReactNode; movable?: boolean }) {
   const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id, disabled: !movable });
-
   const style = {
-    transform: CSS.Transform.toString(transform),
+    transform: CSS.Translate.toString(transform),
     transition,
   };
-
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners} className={`shrink-0 ${movable ? 'cursor-grab' : ''}`}>
       {children}
